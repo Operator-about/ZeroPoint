86fed2ac21d21209761f1705db8246a12d9dc62c
Operator <glebgizko@gmail.com>
2026-03-07 22:10:21 +0300

    07.03.26: Update startup.asm | Обновление startup.asm

--- ASM/startup.asm
+++ ASM/startup.asm
@@ -1,6 +1,7 @@
 .section ".text.boot"
 .global _start
 .global main
+.global switch_EL
 
 _start:
     //Настройка таблицы-векторов
@@ -8,6 +9,8 @@ _start:
     MOV SP, X0
     ADR X0, vector_table
     MSR VBAR_EL1, X0 //Загрузка таблицы векторов
+    MOV X0, #0
+    MSR DAIF, X0 //Обнуление PSTATE
     MSR DAIFCLR, #2
 
     //MRS X0, MPIDR_EL1
@@ -33,6 +36,28 @@ setup_fpu:
     MSR CPACR_EL1, X0
     ISB
 
+switch_EL:
+    MOV X1, X0
+
+    //Конфигурация
+    MRS X0, SCR_EL3
+    ORR X0, X0, #1 //NS
+    ORR X0, X0, #(1 << 10) //RW
+    MSR SCR_EL3, X0
+    ISB
+
+    MRS X0, SPSR_EL3 //Сохранение значение регистра: сохранения статуса программы
+    MSR SPSR_EL3, X0
+
+    MRS X0, ELR_EL3
+    MOV X0, X1
+    MSR ELR_EL3, X0
+    ISB
+
+    ERET
+
+    MSR DAIF, X0 //Обнуление PSTATE
+    MSR DAIFCLR, #2
     
 
 
